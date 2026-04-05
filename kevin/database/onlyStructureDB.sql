6ffb073da77f7c8536fb209892b22e06a11902ae
kevinzancle <kevin.pereira@sptech.school>
2026-04-05 12:47:26 -0300

    add bd structure

---
+++ kevin/database/onlyStructureDB.sql
@@ -0,0 +1,69 @@
+CREATE DATABASE Kelper;
+USE Kelper;
+
+CREATE TABLE endereco (
+    idEndereco INT PRIMARY KEY AUTO_INCREMENT,
+    cep CHAR(8),
+    logradouro VARCHAR(100),
+    bairro VARCHAR(100),
+    cidade VARCHAR(100),
+    estado_sigla CHAR(2),
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE empresa (
+    idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    cnpj CHAR(14) UNIQUE, 
+    fk_matriz INT, 
+    fk_endereco INT,
+    numero VARCHAR(10),
+    complemento VARCHAR(10),
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_empresa_matriz FOREIGN KEY (fk_matriz) REFERENCES empresa(idEmpresa),
+    CONSTRAINT fk_empresa_cep FOREIGN KEY (fk_cep) REFERENCES endereco(idEndereco)
+);
+
+CREATE TABLE usuario (
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    cpf CHAR(14) UNIQUE,
+    senha_hash VARCHAR(255),
+    fk_empresa INT,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updadtedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_usuario_empresa FOREIGN KEY (fk_empresa) REFERENCES empresa(idEmpresa)
+);
+
+CREATE TABLE viveiro (
+    idViveiro INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255),
+    fk_empresa INT,
+    latitude DECIMAL(10, 8), 
+    longitude DECIMAL(11, 8),
+    status_atividade BOOLEAN DEFAULT TRUE,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_viveiro_empresa FOREIGN KEY (fk_empresa) REFERENCES empresa(idEmpresa)
+);
+
+CREATE TABLE sensor (
+    idSensor INT PRIMARY KEY AUTO_INCREMENT,
+    modelo VARCHAR(50), -- Ex: 'Arduino Uno', 'DHT11'
+    fk_viveiro INT,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_sensor_viveiro FOREIGN KEY (fk_viveiro) REFERENCES viveiro(idViveiro)
+);
+
+CREATE TABLE sensor_data(
+    idLeitura INT PRIMARY KEY AUTO_INCREMENT,
+    temperatura DECIMAL(10, 2),
+    luminosidade DECIMAL(10, 2),
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    fk_sensor INT,
+    CONSTRAINT fk_leitura_sensor FOREIGN KEY (fk_sensor) REFERENCES sensor(idSensor)
+);
