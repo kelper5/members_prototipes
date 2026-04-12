2106b747a2ae3ab56695c4e9e9b0aef5cd4e6988
kevinzancle <kevin.pereira@sptech.school>
2026-04-12 17:32:43 -0300

    add style dash

--- kevin/website-codes/scriptDash.js
+++ kevin/website-codes/scriptDash.js
@@ -1,55 +1,79 @@
 const ctx = document.getElementById('graficoLinha');
 
-  new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00'],
-      datasets: [
-        {
-            label: 'Temperatura',
-            data: [30, 29, 28, 25, 22, 23],
-            borderWidth: 1
-        },
-        {
-            label: 'Umidade',
-            data: [80, 82, 80, 85, 80, 83],
-            borderWidth: 1
-        }
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00'],
+    datasets: [
+      {
+        label: 'Luminosidade',
+        data: [30000, 10000, 5000, 25000, 22000, 23000],
+        borderWidth: 1,
+        backgroundColor: '#ddd',
+        borderColor: '#fff',
+        tension: 0.5,
+      },
     ]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          color: '#fff'
+        },
+        grid: {
+          color: '#000'
+        }
+      },
+      x: {
+        ticks: {
+          color: '#fff'
+        },
+        grid: {
+          color: '#000'
         }
       }
     }
-  });
+  }
+});
 
-  const ctx2 = document.getElementById('graficoColuna');
+const ctx2 = document.getElementById('graficoColuna');
 
-  new Chart(ctx2, {
-    type: 'bar',
-    data: {
-      labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
-      datasets: [
-        {
-            label: 'Temperatura Média',
-            data: [22, 24, 27, 23, 20, 18],
-            borderWidth: 1
-        },
-        {
-            label: 'Umidade Média',
-            data: [90, 89, 93, 87, 88, 82],
-            borderWidth: 1
-        }
+new Chart(ctx2, {
+  type: 'bar',
+  data: {
+    labels: ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00'],
+    datasets: [
+      {
+        label: 'Temperatura',
+        data: [22, 24, 27, 23, 20, 18],
+        borderWidth: 1,
+        backgroundColor: '#ddd',
+      },
     ]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          color: '#fff'
+        },
+        grid: {
+          color: '#000'
+        }
+      },
+      x: {
+        ticks: {
+          color: '#fff'
+        },
+        grid: {
+          color: '#000'
         }
       }
     }
-  });
+  }
+});
+
+Chart.defaults.backgroundColor = '#9BD0F5';
