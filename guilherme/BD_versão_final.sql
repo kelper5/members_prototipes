3ff40e865b77e04d7e985eec4dfbff7328643f97
GuiAPolisptech <guilherme.polizello@sptech.school>
2026-04-27 02:40:34 -0300

    Add files via upload

--- guilherme/BD_versão_final.sql
+++ guilherme/BD_versão_final.sql
@@ -1,5 +1,9 @@
+
 CREATE DATABASE Kelper;
 USE Kelper;
+
+
+
 
 CREATE TABLE endereco (
     idEndereco INT PRIMARY KEY AUTO_INCREMENT,
@@ -56,8 +60,14 @@ INSERT INTO usuario
                          (3, 'Pedro', 'pedroemail@email.com', '23463584275', 'senha#DoPedro', 1, default, default),
                          (4, 'Marcos', 'marcosemail@email.com', '02973974536', 'senha#DoMarcos', 3, default, default),
                          (5, 'Gisele', 'giseleemail@email.com', '16726489834', 'senha#DaGisele', 2, default, default);
+						
+                        
+Insert into usuario 
+					values(6, 'Olavio', 'olavioemail@email.com', '73647383263', 'senha#DaOlavio', 4, default, default),
+                         (7, 'Matheus', 'matheusemail@email.com', '09835648372', 'senha#DaMatheus', 5, default, default);
+                         
 
-drop table usuario;
+
 CREATE TABLE viveiro (
     idViveiro INT PRIMARY KEY AUTO_INCREMENT,
     fk_empresa INT NOT NULL,
@@ -70,13 +80,12 @@ CREATE TABLE viveiro (
     CONSTRAINT fk_empresa FOREIGN KEY (fk_empresa) REFERENCES empresa(idEmpresa)
 );
 INSERT INTO viveiro
-				   VALUES(1, 3, 250, 12.09876543, 92.784665324, true, default, default),
-						 (2, 1, 180, 43.68743892, 98.846265325, true, default, default),
-                         (3, 5, 500, 98.36747462, 27.637894622, false, default, default),
-                         (4, 2, 350, 27.74647838, 13.982463947, true, default, default),
-                         (5, 4, 400, 38.74655378, 19.583621899, true, default, default);
-                         
-						
+				   VALUES(1, 3, 250, 12.09876543, 92.78465324, true, default, default),
+						 (2, 1, 180, 43.68743892, 98.84665325, true, default, default),
+                         (3, 5, 500, 98.36747462, 27.63894622, false, default, default),
+                         (4, 2, 350, 27.74647838, 13.92463947, true, default, default),
+                         (5, 4, 400, 38.74655378, 19.83621899, true, default, default);
+                 	
 
 CREATE TABLE arduino (
     idArduino INT PRIMARY KEY AUTO_INCREMENT,
@@ -109,8 +118,9 @@ INSERT INTO arduino_leitura
                                  (3, 18.7, 800, default, 1),
                                  (4, 15.3, 1300, default, 5),
                                  (5, 25.8, 1800, default, 3);
-
-
+                                 
+truncate table arduino_leitura;
+select idLeitura, concat(temperatura, " C º") Temperatura,  luminosidade, createdAt,fk_Arduino From arduino_leitura;
 
        CREATE TABLE alerta(
 	idAlerta 			INT PRIMARY KEY NOT NULL,		
@@ -137,7 +147,7 @@ SELECT * FROM arduino;
 SELECT * FROM arduino_leitura;
 SELECT * FROM alerta;
 
-SELECT em.nome_empresa, u.nome_usuario, u.cpf, v.idViveiro, a.idArduino, al.luminosidade, al.temperatura, ta.nivelRisco
+/*SELECT em.nome_empresa, u.nome_usuario, u.cpf, v.idViveiro, a.idArduino, al.luminosidade, al.temperatura, ta.nivelRisco
 FROM empresa as em
 JOIN usuario as u
 on em.idEmpresa = u.fk_empresa
@@ -148,11 +158,20 @@ on a.fk_viveiro = v.idViveiro
 join arduino_leitura as al
 on al.fk_arduino = a.idArduino
 JOIN alerta as ta
-on ta.fk_arduino_leitura = al.idLeitura; 
+on ta.fk_arduino_leitura = al.idLeitura;*/ 
+
+SELECT em.nome_empresa, u.nome_usuario, v.idViveiro
+FROM empresa as em
+JOIN usuario as u
+ON em.idEmpresa = u.fk_empresa
+JOIN viveiro as v
+ON em.idEmpresa = v.fk_empresa;
+
 
 SELECT  u.*, em.nome_empresa
 FROM empresa AS em
 RIGHT JOIN usuario AS u 
+
 ON em.idEmpresa = u.fk_empresa; 
 
 
