989065ebfa32648ffd587136a751f70548e9b5f0
yugui-matsuyama <lucas.matuyama@sptech.school>
2026-05-07 23:07:39 -0300

    Alteração nas routes

---
+++ yugui/web-data-viz2/src/routes/capturas.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+var capturaController = require("../controllers/capturaController");
+
+router.get("/ultimas/:fk_viveiro", function (req, res) {
+    capturaController.buscarUltimasCapturas(req, res);
+});
+
+router.get("/tempo-real/:fk_viveiro", function (req, res) {
+    capturaController.buscarCapturasEmTempoReal(req, res);
+})
+
+module.exports = router;
