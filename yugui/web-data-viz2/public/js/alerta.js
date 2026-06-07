37e635d4b68c6d7c5459b0e039f7daa988e8a390
yugui-matsuyama <lucas.matuyama@sptech.school>
2026-06-07 04:05:54 -0300

    Implementação do sistema de alerta

--- yugui/web-data-viz2/public/js/alerta.js
+++ yugui/web-data-viz2/public/js/alerta.js
@@ -1,125 +1,138 @@
-var alertas = [];
+let viveiros_alerta = JSON.parse(sessionStorage.ALERTA || "[]");
 
-function obterdados(idAquario) {
-    fetch(`/medidas/tempo-real/${idAquario}`)
+function obterdados(idViveiro, biomassa_media) {
+    fetch(`/capturas/tempo-real/${idViveiro}`)
         .then(resposta => {
             if (resposta.status == 200) {
                 resposta.json().then(resposta => {
+                    let ire = biomassa_media * resposta[0].temperatura * resposta[0].luminosidade / 15000000
 
-                    console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+                    let novoArray = [];
 
-                    alertar(resposta, idAquario);
-                });
-            } else {
-                console.error(`Nenhum dado encontrado para o id ${idAquario} ou erro na API`);
+                    for (let i = 0; i < viveiros_alerta.length; i++) {
+                        if (viveiros_alerta[i].idViveiro !== idViveiro) {
+                            novoArray.push(viveiros_alerta[i]);
+                        }
+                    }
+
+                    if (ire > 0.5) {
+                        novoArray.push({ idViveiro, ire });
+                    }
+
+                    viveiros_alerta = novoArray;
+                    sessionStorage.ALERTA = JSON.stringify(viveiros_alerta);
+                })
+            }
+            else {
+                console.error(`Nenhum dado encontrado para o id ${idViveiro} ou erro na API`);
             }
         })
         .catch(function (error) {
-            console.error(`Erro na obtenção dos dados do aquario p/ gráfico: ${error.message}`);
+            console.error(`Erro na obtenção dos dados do viveiro p/ gráfico: ${error.message}`);
         });
+};
 
-}
 
-function alertar(resposta, idAquario) {
-    var temp = resposta[0].temperatura;
+// function alertar(resposta, idViveiro) {
+//     var temp = resposta[0].temperatura;
 
-    var grauDeAviso = '';
+//     var grauDeAviso = '';
 
-    var limites = {
-        muito_quente: 23,
-        quente: 22,
-        ideal: 20,
-        frio: 10,
-        muito_frio: 5
-    };
+//     var limites = {
+//         muito_quente: 23,
+//         quente: 22,
+//         ideal: 20,
+//         frio: 10,
+//         muito_frio: 5
+//     };
 
-    var classe_temperatura = 'cor-alerta';
+//     var classe_temperatura = 'cor-alerta';
 
-    if (temp >= limites.muito_quente) {
-        classe_temperatura = 'cor-alerta perigo-quente';
-        grauDeAviso = 'perigo quente'
-        grauDeAvisoCor = 'cor-alerta perigo-quente'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
-    }
-    else if (temp < limites.muito_quente && temp >= limites.quente) {
-        classe_temperatura = 'cor-alerta alerta-quente';
-        grauDeAviso = 'alerta quente'
-        grauDeAvisoCor = 'cor-alerta alerta-quente'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
-    }
-    else if (temp < limites.quente && temp > limites.frio) {
-        classe_temperatura = 'cor-alerta ideal';
-        removerAlerta(idAquario);
-    }
-    else if (temp <= limites.frio && temp > limites.muito_frio) {
-        classe_temperatura = 'cor-alerta alerta-frio';
-        grauDeAviso = 'alerta frio'
-        grauDeAvisoCor = 'cor-alerta alerta-frio'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
-    }
-    else if (temp <= limites.muito_frio) {
-        classe_temperatura = 'cor-alerta perigo-frio';
-        grauDeAviso = 'perigo frio'
-        grauDeAvisoCor = 'cor-alerta perigo-frio'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
-    }
+//     if (temp >= limites.muito_quente) {
+//         classe_temperatura = 'cor-alerta perigo-quente';
+//         grauDeAviso = 'perigo quente'
+//         grauDeAvisoCor = 'cor-alerta perigo-quente'
+//         exibirAlerta(temp, idViveiro, grauDeAviso, grauDeAvisoCor)
+//     }
+//     else if (temp < limites.muito_quente && temp >= limites.quente) {
+//         classe_temperatura = 'cor-alerta alerta-quente';
+//         grauDeAviso = 'alerta quente'
+//         grauDeAvisoCor = 'cor-alerta alerta-quente'
+//         exibirAlerta(temp, idViveiro, grauDeAviso, grauDeAvisoCor)
+//     }
+//     else if (temp < limites.quente && temp > limites.frio) {
+//         classe_temperatura = 'cor-alerta ideal';
+//         removerAlerta(idViveiro);
+//     }
+//     else if (temp <= limites.frio && temp > limites.muito_frio) {
+//         classe_temperatura = 'cor-alerta alerta-frio';
+//         grauDeAviso = 'alerta frio'
+//         grauDeAvisoCor = 'cor-alerta alerta-frio'
+//         exibirAlerta(temp, idViveiro, grauDeAviso, grauDeAvisoCor)
+//     }
+//     else if (temp <= limites.muito_frio) {
+//         classe_temperatura = 'cor-alerta perigo-frio';
+//         grauDeAviso = 'perigo frio'
+//         grauDeAvisoCor = 'cor-alerta perigo-frio'
+//         exibirAlerta(temp, idViveiro, grauDeAviso, grauDeAvisoCor)
+//     }
 
-    var card;
+//     var card;
 
-    if (document.getElementById(`temp_aquario_${idAquario}`) != null) {
-        document.getElementById(`temp_aquario_${idAquario}`).innerHTML = temp + "°C";
-    }
+//     if (document.getElementById(`temp_aquario_${idViveiro}`) != null) {
+//         document.getElementById(`temp_aquario_${idViveiro}`).innerHTML = temp + "°C";
+//     }
 
-    if (document.getElementById(`card_${idAquario}`)) {
-        card = document.getElementById(`card_${idAquario}`)
-        card.className = classe_temperatura;
-    }
-}
+//     if (document.getElementById(`card_${idViveiro}`)) {
+//         card = document.getElementById(`card_${idViveiro}`)
+//         card.className = classe_temperatura;
+//     }
+// }
 
-function exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor) {
-    var indice = alertas.findIndex(item => item.idAquario == idAquario);
+// function exibirAlerta(temp, idViveiro, grauDeAviso, grauDeAvisoCor) {
+//     var indice = alertas.findIndex(item => item.idViveiro == idViveiro);
 
-    if (indice >= 0) {
-        alertas[indice] = { idAquario, temp, grauDeAviso, grauDeAvisoCor }
-    } else {
-        alertas.push({ idAquario, temp, grauDeAviso, grauDeAvisoCor });
-    }
+//     if (indice >= 0) {
+//         alertas[indice] = { idViveiro, temp, grauDeAviso, grauDeAvisoCor }
+//     } else {
+//         alertas.push({ idViveiro, temp, grauDeAviso, grauDeAvisoCor });
+//     }
 
-    exibirCards();
-}
+//     exibirCards();
+// }
 
-function removerAlerta(idAquario) {
-    alertas = alertas.filter(item => item.idAquario != idAquario);
-    exibirCards();
-}
+// function removerAlerta(idViveiro) {
+//     alertas = alertas.filter(item => item.idViveiro != idViveiro);
+//     exibirCards();
+// }
 
-function exibirCards() {
-    alerta.innerHTML = '';
+// function exibirCards() {
+//     alerta.innerHTML = '';
 
-    for (var i = 0; i < alertas.length; i++) {
-        var mensagem = alertas[i];
-        alerta.innerHTML += transformarEmDiv(mensagem);
-    }
-}
+//     for (var i = 0; i < alertas.length; i++) {
+//         var mensagem = alertas[i];
+//         alerta.innerHTML += transformarEmDiv(mensagem);
+//     }
+// }
 
-function transformarEmDiv({ idAquario, temp, grauDeAviso, grauDeAvisoCor }) {
+// function transformarEmDiv({ idViveiro, temp, grauDeAviso, grauDeAvisoCor }) {
 
-    var descricao = JSON.parse(sessionStorage.AQUARIOS).find(item => item.id == idAquario).descricao;
-    return `
-    <div class="mensagem-alarme">
-        <div class="informacao">
-            <div class="${grauDeAvisoCor}">&#12644;</div> 
-            <h3>${descricao} está em estado de ${grauDeAviso}!</h3>
-            <small>Temperatura capturada: ${temp}°C.</small>   
-        </div>
-        <div class="alarme-sino"></div>
-    </div>
-    `;
-}
+//     var descricao = JSON.parse(sessionStorage.AQUARIOS).find(item => item.id == idViveiro).descricao;
+//     return `
+//     <div class="mensagem-alarme">
+//         <div class="informacao">
+//             <div class="${grauDeAvisoCor}">&#12644;</div> 
+//             <h3>${descricao} está em estado de ${grauDeAviso}!</h3>
+//             <small>Temperatura capturada: ${temp}°C.</small>   
+//         </div>
+//         <div class="alarme-sino"></div>
+//     </div>
+//     `;
+// }
 
 function atualizacaoPeriodica() {
-    JSON.parse(sessionStorage.AQUARIOS).forEach(item => {
-        obterdados(item.id)
+    JSON.parse(sessionStorage.VIVEIROS).forEach(item => {
+        obterdados(item.idViveiro, item.biomassa_media)
     });
     setTimeout(atualizacaoPeriodica, 5000);
 }
