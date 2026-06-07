57fd3d9cb0cd777085f8368c76b628f5f6447b98
yugui-matsuyama <lucas.matuyama@sptech.school>
2026-06-07 04:05:19 -0300

    Atualização dos controllers do arduino

--- yugui/web-data-viz2/src/controllers/arduinoController.js
+++ yugui/web-data-viz2/src/controllers/arduinoController.js
@@ -1,13 +1,5 @@
 var arduinoModel = require("../models/arduinoModel");
 
-function buscarPorId(req, res) {
-  var id = req.params.id;
-
-  arduinoModel.buscarPorId(id).then((resultado) => {
-    res.status(200).json(resultado);
-  });
-}
-
 function cadastrar(req, res) {
     var idViveiro = req.body.idViveiroServer;
     arduinoModel.cadastrar(idViveiro)
@@ -20,6 +12,5 @@ function cadastrar(req, res) {
 }
 
 module.exports = {
-  buscarPorId,
   cadastrar
 };
